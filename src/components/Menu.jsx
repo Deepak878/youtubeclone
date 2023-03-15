22bf1ca8046bd6fe64fb5e7c437f6943e20462a7
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2023-03-15 21:31:34 +0545

    first part frontend done

---
+++ src/components/Menu.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import styled from "styled-components";
+import MeTube from "../img/logo.png";
+import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+const Container = styled.div`
+  flex: 1;
+  background-color: ${({ theme }) => theme.bg};
+  height: 100vh;
+  color: ${({ theme }) => theme.text};
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+`;
+const Wrapper = styled.div`
+  padding: 5px 20px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 5px;
+  padding: 5px 0px;
+`;
+const Img = styled.img`
+  height: 25px;
+`;
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 8px 0px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.soft};
+  }
+`;
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 10px;
+`;
+const Menu = ({ darkMode, setDarkMode }) => {
+  return (
+    <Container>
+      <Wrapper>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Logo>
+            <Img src={MeTube} />
+            MeTube
+          </Logo>
+        </Link>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
+        <Item>
+          <HomeIcon />
+          Explore
+        </Item>
+        <Item>
+          <HomeIcon />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <HomeIcon />
+          Library
+        </Item>
+        <Item>
+          <HomeIcon />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos,comment and subscribe
+          <Link to="signin" style={{ textDecoration: "none" }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+            </Button>
+          </Link>
+        </Login>
+        <Hr />
+        <Title>Best of METUBE</Title>
+        <Item>
+          <HomeIcon />
+          Music
+        </Item>
+        <Item>
+          <HomeIcon />
+          Sports
+        </Item>
+        <Item>
+          <HomeIcon />
+          Gaming
+        </Item>
+        <Item>
+          <HomeIcon />
+          Movies
+        </Item>
+        <Item>
+          <HomeIcon />
+          News
+        </Item>
+        <Item>
+          <HomeIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <HomeIcon />
+          Settings
+        </Item>
+        <Item>
+          <HomeIcon />
+          Report
+        </Item>
+        <Item>
+          <HomeIcon />
+          Help
+        </Item>
+        <Item onClick={() => setDarkMode(!darkMode)}>
+          <HomeIcon />
+          {!darkMode ? "Dark Mode" : "Light Mode"}
+        </Item>
+        <Item>
+          <HomeIcon />
+          History
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
---
+++ src/pages/Video.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import styled from "styled-components";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+import ThumbDownOutlinedIcon from "@mui/icons-material/ThumbDownOutlined";
+import Comments from "../components/Comments";
+import Card from "../components/Card";
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+const Content = styled.div`
+  flex: 5;
+`;
+const VideoWrapper = styled.div``;
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+const Details = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 18px;
+`;
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+const Buttons = styled.span`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+const Button = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+const Recommendation = styled.div`
+  flex: 2;
+`;
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+`;
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  fontsize: 12px;
+`;
+const Description = styled.p`
+  fontsize: 14px;
+`;
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+const Video = () => {
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>7,948,154 views * Jun 22, 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon /> 123
+            </Button>
+            <Button>
+              <ThumbDownOutlinedIcon />
+              Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+          </ChannelInfo>
+          <ChannelDetail>
+            <ChannelName>Deepak Developer</ChannelName>
+            <ChannelCounter>200k subscribers</ChannelCounter>
+            <Description>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              dignissimos, quam aliquid illo id dolores pariatur aut a nostrum
+              qui beatae ducimus saepe sunt at suscipit alias, quisquam
+              laudantium ipsa.
+            </Description>
+          </ChannelDetail>
+          <Subscribe>Subscribe</Subscribe>
+        </Channel>
+        <Hr />
+        <Comments />
+      </Content>
+      <Recommendation>
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+        <Card type="sm" />
+      </Recommendation>
+    </Container>
+  );
+};
+
+export default Video;
